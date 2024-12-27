297e03e164ee1a556499f9d81a0bbb31b2002caf
omarzydan610 <omarzydan610@gmail.com>
2024-12-27 22:58:13 +0200

    finish collaborators in back

--- backend/routes/tasksRoutes.js
+++ backend/routes/tasksRoutes.js
@@ -12,6 +12,8 @@ router.delete("/delete/:taskId", verifyToken, TaskController.deleteTask);
 router.get("/subTasks/:parentId", verifyToken, TaskController.getSubTasks);
 router.put("/description/:taskId", verifyToken, TaskController.editDescription);
 router.put("/update/:taskId", verifyToken, TaskController.updateTask);
+router.put("/add-collaborate/:taskId/:CollaborateEmail",verifyToken, TaskController.addCollaborate)
+router.get("/get-collaborators/:taskId",verifyToken, TaskController.getCollaborators)
 
 
 module.exports = router;
