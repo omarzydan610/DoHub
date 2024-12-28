1958e187624e3de80451800dca2c8e98c5971e87
abdonaware <abdonawar1234567@gmail.com>
2024-12-28 06:55:34 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/tasksRoutes");
 const userRoutes = require("./routes/userRoutes");
+const sseRoutes = require("./routes/sseRoutes");
 const tagRoutes = require("./routes/tagsRoutes");
 app.use(express.json());
 
@@ -14,13 +15,14 @@ app.use(authRoutes);
 app.use("/user", userRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/tags", tagRoutes);
+app.use("/sse", sseRoutes);
 app.use(errorHandler);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
 const PORT = process.env.PORT ?? 8081;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
